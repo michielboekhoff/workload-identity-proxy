935e93715aeb80549e52318f9f9b97330de5b4e2
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-10-30 06:35:23 +0000

    Create rustls proxy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io,
+    error::Error,
+    io::Write,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -7,8 +8,12 @@ use std::{
 };
 
 use clap::Parser;
+use rustls::{ClientConfig, RootCertStore, ServerName};
 use structured_logger::{async_json::new_writer, Builder};
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    net::TcpListener,
+};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -18,15 +23,34 @@ struct Args {
 
     #[arg(short, long, default_value_t = 5432)]
     port: u16,
+
+    #[arg(short, long)]
+    database_hostname: String,
+}
+
+fn create_tls_conn(server_name: String) -> Result<rustls::ClientConnection, impl Error> {
+    let root_store = RootCertStore::empty();
+    let cfg = ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+
+    let parsed_server_name = ServerName::try_from(server_name.as_str()).unwrap();
+
+    return Ok::<rustls::client::ClientConnection, Box<rustls::Error>>(
+        rustls::ClientConnection::new(Arc::new(cfg), parsed_server_name)?,
+    );
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     Builder::with_level("info")
         .with_target_writer("*", new_writer(tokio::io::stdout()))
         .init();
+
+    let mut conn = create_tls_conn(args.database_hostname)?;
 
     let listener = TcpListener::bind((args.bind_address.clone(), args.port)).await?;
 
@@ -35,7 +59,15 @@ async fn main() -> io::Result<()> {
 
     log::info!("Binding to {} on port {}", args.bind_address, args.port);
     while !term.load(Ordering::Relaxed) {
-        let (_, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
+        let mut reader = BufReader::new(socket);
+        let buf = reader.fill_buf().await?;
+        let mut writer = conn.writer();
+
+        match writer.write_all(buf) {
+            Err(err) => log::error!("Error proxying packet: {}", err),
+            _ => {}
+        }
     }
 
     Ok(())
